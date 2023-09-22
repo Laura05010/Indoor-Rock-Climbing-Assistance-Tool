21c05df19c7da94132e18e2129add32df216f684
laura05010 <lcmmadrid5@gmail.com>
2023-09-22 18:51:45 -0400

    Added bone names

--- BodyPoseTracker/BodyPoseTracker/SkeletonBone.swift
+++ BodyPoseTracker/BodyPoseTracker/SkeletonBone.swift
@@ -13,7 +13,13 @@ struct SkeletonBone{
     var fromJoint: SkeletonJoint
     var toJoint: SkeletonJoint
     
+    // calculate the midpoint between the joints
     var centerPosition: SIMD3<Float>{
-        [(fromJoint.position.x + toJoint.position.x)//2 , ]
+        [(fromJoint.position.x + toJoint.position.x)/2 , (fromJoint.position.y + toJoint.position.y)/2, (fromJoint.position.z + toJoint.position.z)/2]
+    }
+    
+    //Euclidean distance between both points
+    var length: Float {
+        simd_distance(fromJoint.position, toJoint.position)
     }
 }
