94920a9b7667db080b7cdae327e633854feb4691
LucasNH <lucas.noritomi@outlook.com>
2023-11-16 10:15:05 -0500

    realigned and made more efficient

--- audio_feedback.py
+++ audio_feedback.py
@@ -7,14 +7,15 @@ def generate_beep(frequency, duration, volume=0.5):
     # Calculate the time axis
     t = np.linspace(0, duration, int(duration * 44100), False)
     
-    # Generate a sine wave for the given frequency
-    beep = volume * np.sin(2 * np.pi * frequency * t)
-    
-    return beep
+    # Generate a sine wave for the given frequency (beep)
+    return volume * np.sin(2 * np.pi * frequency * t)
 
 def play_beep(beep):
     sd.play(beep)
     sd.wait()
+
+def play(freq=C_NATURAL, dur=1, vol=0.1) -> None:
+    play_beep(generate_beep(freq, dur, vol))
 
 def play_baseline():
     frequency = C_NATURAL
@@ -22,16 +23,12 @@ def play_baseline():
     volume = 0.2
     play(frequency, duration, volume)
 
-def play(freq=C_NATURAL, dur=1, vol=0.1) -> None:
-    beep = generate_beep(freq, dur, vol)
-    play_beep(beep)
-
 def calibrated_sound():
     play(4 * C_NATURAL, 0.5, 0.1)
 
 def main():
     play(4 * C_NATURAL, 0.5, 0.1)
-    # play_baseline()
+    play_baseline()
     frequency = 261.63  # Adjust the frequency in Hz
     duration = 2.2      # Adjust the duration in seconds
     volume = 0.15       # Adjust the volume (0.0 to 1.0)
