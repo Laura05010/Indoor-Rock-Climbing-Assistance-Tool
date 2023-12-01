92fcc538572daf5b608bb2682043528dee7681b9
laura05010 <lcmmadrid5@gmail.com>
2023-11-30 20:55:22 -0500

    added colouring clustering method for single images

--- calibrate.py
+++ calibrate.py
@@ -2,7 +2,7 @@ import cv2
 import supervision as sv
 import time
 
-def calibrate_holds(start_time, detections, model, frame, box_annotator, image, 
+def calibrate_holds(start_time, detections, model, frame, box_annotator, image,
                     calibrated):
     print(f"Calibrating... " + " " * 20, end='\r')
 
@@ -14,11 +14,11 @@ def calibrate_holds(start_time, detections, model, frame, box_annotator, image,
     if elapsed_time <= calibrate_time:
         detections = sv.Detections.from_ultralytics(model(frame,
                                                           verbose=False)[0])
-        detections = detections[detections.confidence > 0.8]
-        frame = box_annotator.annotate(scene=image, detections=detections, 
+        detections = detections[detections.confidence > 0.5]
+        frame = box_annotator.annotate(scene=image, detections=detections,
                                        skip_label=True)
 
-        cv2.putText(frame, "Calibrating...", (50, 50), 
+        cv2.putText(frame, "Calibrating...", (50, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 100), 2)
 
         print(f"Calibrating... " + " " * 20, end='\r')
