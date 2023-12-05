d3fd969b9dfabfcb16f19cbc92f71b1559fd3163
laura05010 <lcmmadrid5@gmail.com>
2023-12-05 01:50:43 -0500

    added better colour ranges

--- colour_test.py
+++ colour_test.py
@@ -11,116 +11,36 @@ red_lower = np.array([0, 100, 100]) # GOOD
 red_upper = np.array([10, 255, 255]) # GOOD
 
 # Orange color range (including lighter tones)
-orange_lower = np.array([1, 190, 200]) # GOOD
-orange_upper = np.array([18, 255, 255]) # GOOD
+orange_lower = np.array([11, 100, 100]) # GOOD
+orange_upper = np.array([20, 255, 255]) # GOOD
 
 # Yellow color range
-yellow_lower = np.array([20, 100, 100]) # GOOD
+yellow_lower = np.array([22, 100, 100]) # GOOD
 yellow_upper = np.array([35, 255, 255]) # GOOD
 
 # Green color range
-green_lower = np.array([40, 50, 100])  # GOOD
-green_upper = np.array([90, 255, 255]) # GOOD
+green_lower = np.array([40, 50, 0])  # GOOD
+green_upper = np.array([88, 255, 255])  # GOOD
 
 # Blue color range
 blue_lower = np.array([95, 50, 50]) # GOOD
 blue_upper = np.array([120, 255, 255]) # GOOD
 
 # Pink color range
-pink_lower = np.array([155, 70, 200]) # GOOD
-pink_upper = np.array([175, 255, 255]) # GOOD
+pink_lower = np.array([165, 50, 70]) # GOOD
+pink_upper = np.array([180, 160, 250]) # GOOD
 
 # Purple color range
-purple_lower = np.array([115, 40, 40])  # GOOD
-purple_upper = np.array([145, 255, 255]) # GOOD
+purple_lower = np.array([121, 50, 30]) # GOOD
+purple_upper = np.array([160, 250, 250]) # GOOD
 
 # White color range
-white_lower = np.array([0, 0, 200]) # GOOD
-white_upper = np.array([180, 30, 255]) # GOOD
+white_lower = np.array([0,0,100]) # GOOD
+white_upper = np.array([180,40,155]) # GOOD
 
 # Black color range
 black_lower = np.array([0, 0, 0]) # GOOD
-black_upper = np.array([180, 55, 40]) # GOOD
-
-
-def visualize_limits(hsv_lower, hsv_upper):
-    lo_square = np.full((10, 10, 3), hsv_lower, dtype=np.uint8) / 255.0
-    do_square = np.full((10, 10, 3), hsv_lower, dtype=np.uint8) / 255.0
-
-    plt.subplot(1, 2, 1)
-    plt.imshow(hsv_to_rgb(do_square))
-    plt.subplot(1, 2, 2)
-    plt.imshow(hsv_to_rgb(lo_square))
-    plt.show()
-
-
-def visualize_hsv_range(hsv_lower, hsv_upper):
-    hue_values = np.linspace(hsv_lower[0], hsv_upper[0], 100)  # Creating a range of hue values
-    saturation = hsv_upper[1]  # Keeping the saturation constant
-    value = hsv_upper[2]  # Keeping the value constant
-
-    # Generating an HSV spectrum using the hue range and constant saturation/value
-    hsv_spectrum = np.array([[hue, saturation, value] for hue in hue_values], dtype=np.uint8)
-    # Converting the HSV spectrum to RGB for visualization
-    rgb_spectrum = cv2.cvtColor(np.reshape(hsv_spectrum, (1, -1, 3)), cv2.COLOR_HSV2RGB)
-    # Reshaping the RGB spectrum for plotting
-    rgb_spectrum = np.reshape(rgb_spectrum, (1, -1, 3))
-
-    plt.imshow(rgb_spectrum)
-    plt.xlabel("Hue")
-    plt.ylabel("Saturation and Value")
-    plt.title("HSV Color Range")
-    plt.show()
-
-
-
-visualize_hsv_range(blue_lower, blue_upper)
-# visualize_limits(black_lower, black_upper)
-
-
-# https://realpython.com/python-opencv-color-spaces/
-
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import hsv_to_rgb
-import cv2
-# COLOR RANGES
-
-# Red color range
-red_lower = np.array([0, 100, 100]) # GOOD
-red_upper = np.array([10, 255, 255]) # GOOD
-
-# Orange color range (including lighter tones)
-orange_lower = np.array([1, 190, 200]) # GOOD
-orange_upper = np.array([18, 255, 255]) # GOOD
-
-# Yellow color range
-yellow_lower = np.array([20, 100, 100]) # GOOD
-yellow_upper = np.array([35, 255, 255]) # GOOD
-
-# Green color range
-green_lower = np.array([40, 50, 100])  # GOOD
-green_upper = np.array([90, 255, 255]) # GOOD
-
-# Blue color range
-blue_lower = np.array([95, 50, 50]) # GOOD
-blue_upper = np.array([120, 255, 255]) # GOOD
-
-# Pink color range
-pink_lower = np.array([155, 70, 200]) # GOOD
-pink_upper = np.array([175, 255, 255]) # GOOD
-
-# Purple color range
-purple_lower = np.array([115, 40, 40])  # GOOD
-purple_upper = np.array([145, 255, 255]) # GOOD
-
-# White color range
-white_lower = np.array([0, 0, 200]) # GOOD
-white_upper = np.array([180, 30, 255]) # GOOD
-
-# Black color range
-black_lower = np.array([0, 0, 0]) # GOOD
-black_upper = np.array([180, 55, 40]) # GOOD
+black_upper = np.array([180, 40, 50]) # GOOD
 
 
 
@@ -144,7 +64,6 @@ def visualize_hsv_range(hsv_lower, hsv_upper):
 
 
 
-visualize_hsv_range(orange_lower, orange_upper)
-# visualize_limits(black_lower, black_upper)
+visualize_hsv_range(white_lower, white_upper)
 
 
